692ef41b4bbfc1dc717b478170064121dd83c448
Mattia <mattia.tummolilllo@gmail.com>
2020-10-21 19:11:13 +0200

    Completamento esercizio

--- js/script.js
+++ js/script.js
@@ -1,27 +1,56 @@
 // INPUT
-   // richiesta nome e cognome
-   var nomeCognome = document.getElementById("nomeCognome");
-
-   // richiesta numero kilometri
-   var numeroKm = document.getElementById("numeroKm");
-
-   // richiesta numero età
-   var numeroEta = document.getElementById("numeroEta");
+   // DICHIARAZIONI VARIABILI GLOBALI
 
    //biglietto
    var biglietto = document.getElementById("biglietto");
 
    // EVENTI
+      // OUTPUT
       // Genera
       var genera = document.getElementById("genera");
       genera.addEventListener("click",
    	  function() {
-          console.log(("Nome e Cognome : "), nomeCognome.value);
-          console.log(("Numero km : "), numeroKm.value);
-          console.log(("Numero età : "), numeroEta.value);
+           // display block biglietto anteprima
+           biglietto.className = "dBlock";
 
-          // display biglietto on
-          biglietto.className = "dBlock";
+           // DICHIARAZIONE VARIABILI
+            // richiesta numero kilometri
+            var numeroKm = document.getElementById("numeroKm");
+            // richiesta nome e cognome
+            var nomeCognome = document.getElementById("nomeCognome");
+            // richiesta numero età
+            var numeroEta = document.getElementById("numeroEta");
+            var sconto;
+            var tipoSconto = document.getElementById("tipoSconto");
+            var prezzoFinale;
+            var prezzoBiglietto = (0.21 * numeroKm.value).toFixed(2);
+
+            // CALCOLO SCONTI E PREZZO FINALE
+           if (numeroEta.value < 18) { // sconto under 18
+             sconto = (prezzoBiglietto / 100) * 20; //sconto minorenni del 20%
+             prezzoFinale = (prezzoBiglietto - sconto).toFixed(2) + (" €") + (" (-20%)");
+             tipoSconto.value = ("Sconto minorenne applicato");
+          } else if (numeroEta.value > 65){  //sconto over 65
+             sconto = (prezzoBiglietto / 100) * 40; //sconto over 65 del 40%
+             prezzoFinale = (prezzoBiglietto - sconto).toFixed(2) + (" €") + (" (-40%)");
+             tipoSconto.value = ("Sconto over 65 applicato");
+          } else {
+             prezzoFinale = prezzoBiglietto + (" €");
+             tipoSconto.value = ("Nessuno sconto applicato");
+          }
+
+          // output tipo di sconto
+          document.getElementById("tipoSconto").innerHTML = tipoSconto.value;
+          // output costo biglietto
+          document.getElementById("prezzo").innerHTML = prezzoFinale;
+          // output nome e cognome
+          document.getElementById("nomeCognomeB").innerHTML = nomeCognome.value;
+          // output numero carrozza
+           var numeroCarrozza = Math.floor(Math.random()*10);
+           document.getElementById("numeroCarrozza").innerHTML = numeroCarrozza;
+          // output codice CP
+          var numeroCP = 90000 + (Math.floor(Math.random()*10000));
+          document.getElementById("codiceCP").innerHTML = numeroCP;
    	   }
       );
 
@@ -35,69 +64,3 @@
            biglietto.className = "dNone";
    	   }
       );
-
-
-
-
-// prezzo biglietto
-// var prezzoBiglietto = (0.21 * numeroKm).toFixed(2);
-
-
-// // SCONTI
-// if (numeroEta < 18) { // sconto under 18
-//    sconto = (prezzoBiglietto / 100) * 20; //sconto minorenni del 20%
-//    prezzoFinale = (prezzoBiglietto - sconto).toFixed(2) + (" €") + (" (-20%)");
-// } else if (numeroEta >= 65){  //sconto over 65
-//    sconto = (prezzoBiglietto / 100) * 40; //sconto over 65 del 40%
-//    prezzoFinale = (prezzoBiglietto - sconto).toFixed(2) + (" €") + (" (-40%)");
-// } else {
-//    prezzoFinale = prezzoBiglietto + (" €");
-// }
-
-
-// OUTPUT
-document.getElementById("nomeCognomeB").innerHTML = nomeCognome.value; //non ho capito come far uscire il nome inserito nell'input nell'id nomeCognomeB all'interno del biglietto
-
-
-
-
-
-
-
-
-
-
-
-// INPUT
-   // sconto
-   // var sconto;
-   // var prezzoFinale;
-   // richiesta numero età passeggero
-   // var numeroEta = parseInt(prompt("Benvenuto! Quanti anni hai?"));
-   // console.log(numeroEta);
-
-   // richiesta numero kilometri
-   // var numeroKm = parseInt(prompt("Per quanti kilometri devi viaggiare?"));
-   // console.log(numeroKm);
-
-// prezzo biglietto
-// var prezzoBiglietto = (0.21 * numeroKm).toFixed(2);
-// console.log(prezzoBiglietto);
-
-// // SCONTI (con approssimazione a due decimali)
-// if (numeroEta < 18) { // sconto under 18
-//    sconto = (prezzoBiglietto / 100) * 20; //sconto minorenni del 20%
-//    prezzoFinale = (prezzoBiglietto - sconto).toFixed(2) + (" €") + (" (-20%)"); //approssimazione
-//    // console.log(("Valore sconto: " + sconto));
-//    // console.log("Prezzo con sconto del 20% : " + prezzoFinale);
-// } else if (numeroEta >= 65){  //sconto over 65
-//    sconto = (prezzoBiglietto / 100) * 40; //sconto over 65 del 40%
-//    prezzoFinale = (prezzoBiglietto - sconto).toFixed(2) + (" €") + (" (-40%)"); //approssimazione
-//    // console.log(("Valore sconto: " + sconto));
-//    // console.log(("Prezzo con sconto del 40% : ") + prezzoFinale);
-// } else {
-//    prezzoFinale = prezzoBiglietto + (" €");
-// }
-//
-// // OUTPUT
-// document.getElementById("prezzo").innerHTML = prezzoFinale;
